964da701cead9de21f03fada68ee6577837c46b8
Rudolf Hersen <rhersen@gmail.com>
2013-09-29 16:43:59 +0200

    cleanup

--- js/init.js
+++ js/init.js
@@ -1,32 +1,22 @@
 function init() {
-    var canvasEl = document.getElementById("canvas");
-    scene.init(canvasEl);
+    var canvas = $('#canvas');
+    scene.init(canvas.get(0));
 
     function canvasResize() {
-        var container = $('#container');
         var left = $('#left');
-        var content = $('#content');
-        console.log("left.width()=",left.width(),"left.innerWidth()=",left.innerWidth(),"left.outerWidth()=",left.outerWidth());
-        $('#canvas').width(container.width() - left.outerWidth(true) - 24);
-        $('#canvas').height($(window).height() - 24);
-//					$('#left').height($(window).height() - 54);
+        console.log("left.width()=", left.width(), "left.innerWidth()=", left.innerWidth(), "left.outerWidth()=", left.outerWidth());
+        canvas.width($('#container').width() - left.outerWidth(true) - 24);
+        canvas.height($(window).height() - 24);
         scene.setSizes();
     }
 
     canvasResize();
 
-    $(window).bind("resize", function(){
-        canvasResize();
-    });
+    $(window).bind("resize", canvasResize);
 
-    $(function () {
-        $("#left").resizable({
-            handles: 'e',
-            minWidth: '50',
-            maxWidth: '350',
-            resize: function() {
-                canvasResize();
-            }
-        });
+    $("#left").resizable({
+        handles: 'e',
+        minWidth: '50',
+        maxWidth: '350'
     });
 }
